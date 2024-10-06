ef13a0fed353376f16cbcdbecef58bf5f1f45c41
Anjany Pandey <anjany.pandey06@gmail.com>
2024-10-06 18:43:42 +0530

    css fixed

--- client/src/components/Homepage/Homepage.jsx
+++ client/src/components/Homepage/Homepage.jsx
@@ -129,10 +129,12 @@ const Homepage = () => {
     console.log(filePath);
   };
   return (
-    <>
-      <nav className="flex justify-between px-10 py-3 ">
-        <h1 className="text-2xl font-bold">Documents Management System</h1>
-        <div className="flex space-x-5">
+    <div className="container mx-auto p-4 md:p-6 lg:p-8">
+      <nav className="flex justify-between items-center py-3 md:py-6 lg:py-8">
+        <h1 className="text-2xl font-bold md:text-3xl lg:text-4xl">
+          Documents Management System
+        </h1>
+        <div className="flex space-x-5 md:space-x-10 lg:space-x-12">
           {/* <form className="flex justify-between row"> */}
           <input
             type="file"
@@ -167,10 +169,12 @@ const Homepage = () => {
           />
         </div>
       </nav>
-      <hr />
-      <section className="px-10 py-3 gap-3 flex flex-col">
-        <h1 className="text-xl font-semibold">All Files</h1>
-        <div className="border-[1px] items-center p-2 rounded-xl flex gap-1 w-80">
+      <hr className="my-4 md:my-6 lg:my-8" />
+      <section className="px-4 py-6 md:px-6 lg:px-8">
+        <h1 className="text-xl font-semibold md:text-2xl lg:text-3xl">
+          All Files
+        </h1>
+        <div className="border-[1px] items-center p-2 rounded-xl flex gap-1 w-full md:w-1/2 lg:w-1/3">
           <IoSearch size={25} />
           <input
             type="text"
@@ -180,8 +184,8 @@ const Homepage = () => {
             onChange={handleSearch}
           />
         </div>
-        <div>
-          <div className="flex justify-between px-3 py-2 bg-slate-100 text-lg">
+        <div className="mt-4 md:mt-6 lg:mt-8">
+          <div className="flex justify-between px-3 py-2 bg-slate-100 text-lg md:text-xl lg:text-2xl">
             <div>Name</div>
             <div>Description</div>
             <div>Size</div>
@@ -190,7 +194,7 @@ const Homepage = () => {
           {filteredDocuments.map((file) => (
             <div
               key={file.id}
-              className="flex justify-between items-center px-3  "
+              className="flex justify-between items-center px-3 py-2 md:py-4 lg:py-6 "
             >
               {/* <div className="flex gap-2 justify-center items-center "> */}
               {/* <img className="w-8" src="./images/files-logo.png" alt="" /> */}
@@ -220,10 +224,10 @@ const Homepage = () => {
               </div>
             </div>
           ))}
-          <hr />
+          <hr className="my-4 md:my-6 lg:my-8" />
         </div>
       </section>
-    </>
+    </div>
   );
 };
 
